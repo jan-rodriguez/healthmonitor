2639e11a239404f359a6aee7cff02b52a4c1de44
Gabriel Frattallone <Gabo@Gabriels-MacBook-Pro.local>
2014-05-05 17:27:31 -0400

    Login and Navbar

--- healthmonitor.js
+++ healthmonitor.js
@@ -1,15 +1,103 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to healthmonitor.";
-  };
+Router.onBeforeAction(function(pause) {
+  if (!Meteor.user()) {
+    pause();
+    Router.go('root');
+  }
+}, {except: ['root', 'signup']});
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+Router.onBeforeAction(function(pause) {
+  if (Meteor.user()) {
+    pause();
+    Router.go('root');
+  }
+}, {only: ['signup']});
+
+Router.map(function () {
+  this.route('root', {
+    path: '/',
+    template: 'root'
+  });
+
+  this.route('signup', {
+    path: '/signup',
+    template: 'signup'
+  });
+
+  this.route('home', {
+    path: '/home',
+    template: 'home'
+  });
+});
+
+
+if (Meteor.isClient) {
+  currentUser = Meteor.user();
+
+  UI.registerHelper("currentPage", function(localPath) {
+    return Router.current(true).path === localPath;
+  });
+
+  Template.login.events({
+    'submit #login-form' : function(e, t) {
+      e.preventDefault();
+
+      var email = t.find('#login-email').value;
+      var password = t.find('#login-password').value;
+      $('#login-error').html('');
+
+      Meteor.loginWithPassword(email, password, function(err) {
+        if (err) {
+          $('#login-error').html('<div class="alert alert-warning error">Your username or password was incorrect</div>');
+          $('#login-password').val('');
+        } else {
+          Router.go('home');
+        }
+      });
+      return false; 
     }
   });
+
+  Template.signup.events({
+    'submit #signup-form' : function(e, t) {
+      e.preventDefault();
+
+      var email = t.find('#signup-email').value;
+      var password = t.find('#signup-password').value;
+      var confirm_password = t.find('#signup-confirm-password').value;
+      $('#signup-error').html('');
+
+      if (password != confirm_password) {
+        $('#signup-error').html('<div class="alert alert-warning error">Your password did not match</div>');
+        $('#signup-password').val('');
+        $('#signup-confirm-password').val('');
+        return false;
+      } 
+
+      Accounts.createUser({email: email, password : password}, function(err){
+          if (err) {
+            var email_val = $('#signup-email').val();
+            $('#signup-error').html('<div class="alert alert-warning error">' + email_val + ' is already in use</div>');
+            $('#signup-email').val('');
+            $('#signup-password').val('');
+            $('#signup-confirm-password').val('');
+          } else {
+            Router.go('home');
+          }
+      });
+      return false;
+    }
+  });
+
+  Template.navbar.events({
+    'click #logout' : function(e, t) {
+      e.preventDefault()
+
+      Meteor.logout( function() {
+        Router.go('root');
+      });
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
